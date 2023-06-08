1e313d35ef55b7ceccef747d7e0178a30fc49839
Loi Tran <loitran454@gmail.com>
2023-06-08 02:08:08 -0700

    updated generator to generate both numbers and letters

--- src/BullsCows.java
+++ src/BullsCows.java
@@ -1,45 +1,47 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class BullsCows {
     protected int bullCount;
     protected int cowCount;
-    protected List<Integer> secretCode = generateCode();
 
     public BullsCows() {
         this.bullCount = 0;
         this.cowCount = 0;
     }
     public void start() {
-        // game log
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the length of the secret code:");
+        int length = Integer.parseInt(scanner.nextLine());
+        System.out.println("Input the number of possible symbols in the code:");
+        int numberOfSymbols = Integer.parseInt(scanner.nextLine());
+        prepared(length, numberOfSymbols);
+
+        // generate secret code and split it into array
+        String secret = generateCode(length, numberOfSymbols);
+        String[] secretCode = secret.split("");
+
+
         int turn = 0;
-
-
-        System.out.println("The secret code is prepared: ****.\n");
         while (true) {
-            turn++;
-            System.out.println("Turn " + turn + ". Answer: ");
-
-            // generate a different code each loop
-            List<Integer> code = generateCode();
-            // print that generated code
-            System.out.println(print(code));
+            System.out.print("Turn " + ++turn + ":\n");
+            String answer = scanner.nextLine();
+            String[] answerCode = answer.split("");
 
 
-            // compare the two lists -- check if elements bull or cow
-            for (int i = 0; i < code.size(); i++) {
-                if (code.get(i).equals(secretCode.get(i))) {
+            for (int i = 0; i < answerCode.length; i++) {
+                if (answerCode[i].equals(secretCode[i])) {
                     bullCount++;
                 }
-
-                if (secretCode.contains(code.get(i)) && !(code.get(i).equals(secretCode.get(i)))) {
+                if (secret.contains(answerCode[i]) && !answerCode[i].equals(secretCode[i])) {
                     cowCount++;
                 }
             }
-            // check grade with passed bullCount and cowCount
+
             checkGrade(bullCount, cowCount);
-            if (bullCount == 4) {
+            if (bullCount == length) {
+                System.out.printf("Congratulations! You guessed the secret " +
+                        "code %s.", secret);
                 break;
             }
             bullCount = 0;
@@ -49,75 +51,103 @@ public class BullsCows {
     // check grade and print it
     public void checkGrade(int bullCount, int cowCount) {
 
-        String bulls = "bulls.";
-        String cows = "cows.";
-
-        // if there's NO bulls or cows
-        if (bullCount == 0 && cowCount == 0) {
-            System.out.println("\nNone.\n");
-        }
-
         if (bullCount > 0 && cowCount > 0) {
-            if (bullCount == 1 && cowCount == 1) {
-                System.out.println("Grade: " + bullCount + " bull and " + cowCount + " cow.\n");
-            } else if (bullCount > 1 && cowCount == 1) {
-                System.out.println("Grade: " + bullCount + " bulls and " + cowCount + " cow.\n");
+            if (bullCount > 1 && cowCount > 1) {
+                System.out.printf("Grade: %d bulls and %d cows.\n", bullCount, cowCount);
+            } else if (bullCount > 1) {
+                System.out.printf("Grade: %d bulls and %d cow.\n", bullCount, cowCount);
+            } else if (cowCount > 1) {
+                System.out.printf("Grade: %d bull and %d cows.\n", bullCount, cowCount);
             } else {
-                System.out.println("Grade: " + bullCount + " bull and " + cowCount + " cows.\n");
+                System.out.printf("Grade: %d bull and %d cow.\n", bullCount, cowCount);
             }
+        } else if (bullCount > 0) {
+            System.out.printf(bullCount == 4 ? "Grade: 4 bulls\n" : bullCount < 4 ? "Grade: %d bulls\n" : "Grade: %d bull\n", bullCount);
+        } else if (cowCount > 0) {
+            System.out.printf("Grade: %d cow(s).\n", cowCount);
+        } else {
+            System.out.println("Grade: None.\n");
         }
 
-        // if only bull
-        if (bullCount > 0 && cowCount == 0) {
-            // if more than 1 bull
-            if (bullCount == 1) {
-                System.out.println("Grade: " + bullCount + " bull.\n" );
-            } else if (bullCount < 4) {
-                System.out.println("\nGrade: " + bullCount + " " + bulls +
-                        "\n");
-            }
-            // if 4 bulls in total - win game --> end game
-            if (bullCount == 4) {
-                System.out.println("Grade: " + bullCount + " " + bulls +
-                        "\nCongrats! The secret code is " + print(secretCode) +
-                        "\n");
-            }
-        }
-
-        // if only cow
-        if (bullCount == 0 && cowCount > 0) {
-            if (cowCount > 1) {
-                System.out.println("Grade: " + cowCount + " " + cows +
-                        "\n");
-            } else {
-                System.out.println("Grade: " + cowCount + " cow.\n");
-            }
-        }
-
-    }
-
-    // print elements in list
-    public String print(List<Integer> list) {
-        StringBuilder num = new StringBuilder();
-        for (int i : list) {
-            num.append(i);
-        }
-        return num.toString();
     }
 
     // generate unique 4-digit code and return the list
-    public List<Integer> generateCode() {
-        // generate unique 4-digit code
-        Random random = new Random();
-        // add each number to list
-        List<Integer> list = new ArrayList<>();
+    public String generateCode(int length, int numberOfSymbols) {
+        Random rnd = new Random();
+        StringBuilder str = new StringBuilder();
 
-        while (list.size() < 4) { // generate 4 numbers
-            int rand = random.nextInt(9) + 1; // generate b/t 1 and 9
-            if (!list.contains(rand)) {
-                list.add(rand);
+        final int numbers = 10;
+        int charNum = numberOfSymbols - numbers;
+
+
+        if (numberOfSymbols > 10) {
+            for (int i = str.length(); i < length;) {
+                // generate random number OR letter
+                boolean generateNumber = rnd.nextBoolean();
+                if (generateNumber) {
+                    // store random number to string
+                    int number = rnd.nextInt(numbers);
+                    // if str does not have the number/letter --> add to str
+                    if (str.toString().contains(String.valueOf(number))) {
+                        continue;
+                    }
+                    str.append(number);
+                } else {
+                    // random letter
+                    char letter = (char) (rnd.nextInt(charNum) + 'a');
+                    // if str already contains the random number -->
+                    if (str.toString().contains(String.valueOf(letter))) {
+                        continue;
+                    }
+                    str.append(letter);
+                }
+                i++;
+            }
+        } else {
+            for (int i = 0; i < length;) {
+                int number = rnd.nextInt(numbers);
+                // if str does not have the number/letter --> add to str
+                if (str.toString().contains(String.valueOf(number))) {
+                    continue;
+                }
+                str.append(number);
+                i++;
             }
         }
-        return list;
+
+        return str.toString();
+    }
+
+    public static void prepared(int length, int numberOfSymbols) {
+        String stars = stars(length);
+        String startGame = "\nOkay, let's start a game!\n";
+
+        if (numberOfSymbols == 11) {
+            System.out.printf("The secret is prepared: %s " + "(0-9, a)" +
+                    "%s", stars, startGame);
+        } else if (numberOfSymbols > 11) {
+            System.out.printf("The secret is prepared: %s " + "(0-9, " + "a" +
+                    "-%c)" + "%s" ,stars, characters(numberOfSymbols), startGame);
+        } else if (numberOfSymbols == 10) {
+            System.out.printf("The secret is prepared: %s " + "(0-9)" + "%s",
+                    stars, startGame);
+        } else {
+            System.out.printf("The secret is prepared: %s " + "(0-" + (numberOfSymbols) +
+                    ")" + "%s", stars, startGame);
+        }
+    }
+
+    public static String stars(int length) {
+        StringBuilder star = new StringBuilder("*");
+        int i = 1;
+        while (i < length) {
+            star.append("*");
+            i++;
+        }
+        return star.toString();
+    }
+
+    public static char characters(int numberOfSymbols) {
+        return (char) ('a' + numberOfSymbols - 11);
     }
 }
